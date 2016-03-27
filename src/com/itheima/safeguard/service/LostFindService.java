43f836bdaa80e9cafd6001b95b721475618e7484
GitHub-CPM <ming.2244@gmail.com>
2016-03-28 01:23:56 +0800

    新增了:1.监测SIM卡信息的改变,发送sim卡信息到安全号码;2.使用GPS定位到手机所在位置,并将位置发送到安全号码;3.远程设密码锁屏;4.远程清除sdcard的数据;5.发送短信自动播放音乐.

--- src/com/itheima/safeguard/service/LostFindService.java
+++ src/com/itheima/safeguard/service/LostFindService.java
@@ -6,16 +6,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.telephony.SmsMessage;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * @author CPM
- *
- *	这是一个用于开启手机防盗服务的类,用于后台开启监听短信内容,发短信播放音乐,格式化sdCard等操作.
+ * 
+ *         这是一个用于开启手机防盗服务的类,用于后台开启监听短信内容,发短信播放音乐,格式化sdCard等操作.
  */
 public class LostFindService extends Service {
 
@@ -25,11 +25,11 @@ public class LostFindService extends Service {
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
-	
+
 	/**
 	 * @author CPM
-	 *
-	 *	自定义的短信广播接收者
+	 * 
+	 *         自定义的短信广播接收者
 	 */
 	private class SMSReceiver extends BroadcastReceiver {
 
@@ -50,37 +50,47 @@ public class LostFindService extends Service {
 				// 监听信息的内容,如果信息内容有特殊含义,如
 				// #*gps*# 激活GPS定位功能
 				// #*lockscreen*# 远程锁屏
-				// #*wipedata*#  远程清除数据
-				// #*music*#  音乐报警
-				if(messageBody.equals("#*gps*#")) {
+				// #*wipedata*# 远程清除数据
+				// #*music*# 音乐报警
+				if (messageBody.equals("#*gps*#")) {
 					Log.i("System.out", "监听到了3");
 					// 启动gps后台服务,监听位置变化,并发送具体经纬度到安全号码上
-					Intent gpsIntent = new Intent(context,GPSService.class);
+					Intent gpsIntent = new Intent(context, GPSService.class);
 					startService(gpsIntent);
 					abortBroadcast();// 终止广播
-				} else if(messageBody.equals("#*lockscreen*#")) {
-					// 远程锁屏
-					// 获得设备管理器
+				} else if (messageBody.equals("#*lockscreen*#")) {
 					DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Service.DEVICE_POLICY_SERVICE);
 					dpm.resetPassword("222", 0); // 重设密码
 					dpm.lockNow(); // 一键锁屏
 					abortBroadcast();// 终止广播
+				} else if (messageBody.equals("#*wipedata*#")) {
+					// 远程清除sdcard数据
+					DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Service.DEVICE_POLICY_SERVICE);
+					dpm.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
+					abortBroadcast();// 终止广播
+				} else if (messageBody.equals("#*music*#")) {
+					MediaPlayer mp = MediaPlayer.create(context,
+							com.itheima.safeguard.R.raw.test2);
+					mp.setVolume(1.0F, 1.0F);//防止别人偷了手机之后调小声.
+					mp.start(); // 播放音乐
+					abortBroadcast();// 终止广播
 				}
-				
+
 			}
 		}
-		
+
 	}
-	
+
 	@Override
 	public void onCreate() {
 		smsReceiver = new SMSReceiver();
-		IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");//意图为对短信接收进行监听
-		filter.setPriority(Integer.MAX_VALUE);//优先级设计为最高级别
+		IntentFilter filter = new IntentFilter(
+				"android.provider.Telephony.SMS_RECEIVED");// 意图为对短信接收进行监听
+		filter.setPriority(Integer.MAX_VALUE);// 优先级设计为最高级别
 		registerReceiver(smsReceiver, filter); // 注册接收者
 		super.onCreate();
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		unregisterReceiver(smsReceiver);// 销毁短信接收者
