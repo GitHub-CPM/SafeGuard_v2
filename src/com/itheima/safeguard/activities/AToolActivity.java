a490ea349ba34a110a28fdcc799c8d93a529458c
GitHub-CPM <ming.2244@gmail.com>
2016-04-01 21:29:53 +0800

    增加了以Json格式备份短信到本地的方法和解析备份到本地的Json格式的文件的方法.

--- src/com/itheima/safeguard/activities/AToolActivity.java
+++ src/com/itheima/safeguard/activities/AToolActivity.java
@@ -7,7 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.itheima.safeguard.R;
-import com.itheima.safeguard.engine.SMSBackup;
+import com.itheima.safeguard.engine.SMSEngine;
+import com.itheima.safeguard.engine.SMSEngine.smsBackupRestoreRecall;
 
 /**
  * @author CPM
@@ -30,22 +31,91 @@ public class AToolActivity extends Activity {
 	 */
 	private void initView() {
 		setContentView(R.layout.activity_atool);
-		
+
 		pd = new ProgressDialog(AToolActivity.this);
 		pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);// 横条进度
 	}
 	
-	/**对手机短信进行本地备份的功能
+	/**将备份到本地的数据进行还原
+	 * @param view
+	 */
+	public void restoreSMS(View view) {
+		SMSEngine.smsRestoreJson(AToolActivity.this, new smsBackupRestoreRecall() {
+
+			@Override
+			public void show() {
+				pd.show();
+			}
+			
+			@Override
+			public void setProgress(int progress) {
+				pd.setProgress(progress);
+			}
+			
+			@Override
+			public void setMax(int count) {
+				pd.setMax(count);
+			}
+			
+			@Override
+			public void end() {
+				pd.dismiss();
+			}
+		});
+	}
+
+	/**
+	 * 对手机短信进行本地备份的功能
+	 * 
 	 * @param view
 	 */
 	public void backupSMS(View view) {
-		new Thread(new Runnable() {
-			// 开启子线程,因为备份数据属于耗时操作
+		SMSEngine.smsBackupJson(AToolActivity.this, new smsBackupRestoreRecall() {
+			
 			@Override
-			public void run() {
-				SMSBackup.SMSBackupToPhone(AToolActivity.this,pd);
+			public void show() {
+				pd.show();
 			}
-		}).start();
+			
+			@Override
+			public void setProgress(int progress) {
+				pd.setProgress(progress);
+			}
+			
+			@Override
+			public void setMax(int count) {
+				pd.setMax(count);
+			}
+			
+			@Override
+			public void end() {
+				pd.dismiss();
+			}
+		});
+		
+		/*SMSEngine.smsBackupToPhone(AToolActivity.this,
+				new smsBackupRestoreRecall() {
+
+					@Override
+					public void show() {
+						pd.show(); // 显示进度条
+					}
+
+					@Override
+					public void setProgress(int progress) {
+						pd.setProgress(progress); // 显示进度条的进度
+					}
+
+					@Override
+					public void setMax(int count) {
+						pd.setMax(count); // 设置进度条最大值
+					}
+
+					@Override
+					public void end() {
+						pd.dismiss(); // 让进度条消失
+					}
+				});*/
 	}
 
 	/**
